45a0b3affe7fd423483bf7c1ee71df4fa616cb0c
Natalia Samoilova <nazzzot@gmail.com>
2019-10-23 15:29:43 +0300

    +

--- js2.js
+++ js2.js
@@ -60,53 +60,53 @@ let a = prompt("Введите значение от 0 до 15");
 
 switch (a){
     case "0":
-        console.log(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("0 ");
+    
     case "1":
-        console.log(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("1 ");
+    
     case "2":
-        console.log(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("2 ");
+    
     case "3":
-        console.log(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("3 ");
+    
     case "4":
-        console.log(4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("4 ");
+    
     case "5":
-        console.log(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("5 ");
+    
     case "6":
-        console.log(6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("6 ");
+    
     case "7":
-        console.log(7, 8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("7 ");
+    
     case "8":
-        console.log(8, 9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("8 ");
+    
     case "9":
-        console.log(9, 10, 11, 12, 13, 14, 15);
-        break;
+        console.log("9 ");
+    
     case "10":
-        console.log(10, 11, 12, 13, 14, 15);
-        break;
+        console.log("10 ");
+    
     case "11":
-        console.log(11, 12, 13, 14, 15);
-        break;
+        console.log("11 ");
+    
     case "12":
-        console.log(12, 13, 14, 15);
-        break;
+        console.log("12 ");
+    
     case "13":
-        console.log(13, 14, 15);
-        break;
+        console.log("13 ");
+    
     case "14":
-        console.log(14, 15);
-        break;
+        console.log("14 ");
+    
     case "15":
-        console.log(15);
-        break;
+        console.log("15 ");
+    
     default:
         console.log("Попробуйте снова. Введите значение от 0 до 15");
 }
